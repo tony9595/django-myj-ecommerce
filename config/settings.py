814a07874ee0c23cb734f817d4b3df63c74e63ec
youngjeaMo <tony9595@naver.com>
2025-03-27 11:19:07 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "store",  # dev_1
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # dev_1
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "store/home.html", {})
+    # return HttpResponse("<h1>개발페이지</h1>")
