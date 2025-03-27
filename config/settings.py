07c79a6746ad99e5a0eb91b81408fe38cc87d569
youngjeaMo <tony9595@naver.com>
2025-03-27 11:43:45 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -116,9 +116,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+# dev_2
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+# dev_2
+# http://127.0.0.1:8000/media/파일경로
+MEDIA_URL = "media/"  # ex) /media/photo1.png
+MEDIA_ROOT = os.path.join(BASE_DIR, "medi")
