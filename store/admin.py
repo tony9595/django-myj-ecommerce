b03f72ffa98d2f9d3aa0889067eaa7b9e64f7293
youngjeaMo <tony9595@naver.com>
2025-03-27 14:28:57 +0900

    commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Category, Probuct
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Probuct)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# dev_3
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Probuct(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=250, default="", blank=True, null=True)
+    image = models.ImageField(upload_to="upload/product")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
