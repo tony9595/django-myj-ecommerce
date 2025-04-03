b5c888523628e81d7b14d9daa0dc6613d2a35482
youngjeaMo <tony9595@naver.com>
2025-04-03 13:50:21 +0900

    commit

--- cart/views.py
+++ cart/views.py
@@ -1,24 +1,35 @@
-from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from cart.cart import Cart
 from store.models import Probuct
+from django.http import JsonResponse
 
 
 # Create your views here.
+
+
+# dev_15
 def add_cart(request):
     cart = Cart(request)
 
-    print("카트===", cart)
+    print("카트======", cart)
 
     if request.POST.get("action") == "post":
+
+        # 상품 받아오기
         product_id = int(request.POST.get("product_id"))
-        print("Product_id", product_id)
+        print("product_id", product_id)
 
+        # 상품 개수
         product_qty = int(request.POST.get("product_qty"))
-        print("Product_qty", product_qty)
+        print("product_qty", product_qty)
 
-        product = get_object_or_404(product, id=product_id)
+        # DB에서 찾아서 product 객체로 변환
+        product = get_object_or_404(Probuct, id=product_id)
 
+        # 세션에 저장
         cart.add(product, product_qty)
 
-        return JsonResponse({"상품": product_id})
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
+        response = JsonResponse({"qty": cart_quantity})
+        return response
