010f1ff5f893f3c2c4484353a97571390e549348
youngjeaMo <tony9595@naver.com>
2025-04-08 09:19:14 +0900

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -31,3 +31,6 @@ class User(AbstractUser):
 
     creatd_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # dev_23
+    old_cart = models.CharField(max_length=200, blank=True, null=True)
