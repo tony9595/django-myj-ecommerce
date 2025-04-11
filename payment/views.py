5aa37c28dc109fd3b436e9394b33e50e8bdd910c
youngjeaMo <tony9595@naver.com>
2025-04-11 10:47:30 +0900

    commit

--- payment/views.py
+++ payment/views.py
@@ -10,6 +10,8 @@ from payment.models import Payment
 from store.models import Product
 from django.http import HttpResponse
 
+from orders.forms import ShippingForm
+
 
 # Create your views here.
 # dev_26
@@ -18,8 +20,8 @@ def payment_process(request):
 
     # 결제 성공 시 로직
     # 1.주문 정보 저장을 위해 ajax 요청
-    # 2.서버에서 결제금액과 주문금액(세션에 저장되어있는)이 일치하는지 확인
-    # 3.일치하면 주문 정보 및 결제정보 저장
+    # 2.서버에서 결재금액과 주문금액(세션에 저장되어있는)이 일치하는지 확인
+    # 3.일치하면 주문 정보 및 결재정보 저장
     if request.POST:
 
         cart = Cart(request)
@@ -48,7 +50,17 @@ def payment_process(request):
 
                 create_order_item.save()
 
-            # 결제 정보 저장
+            # dev_26_2
+            # 배송지 정보 저장
+            form = ShippingForm(request.POST)
+
+            if form.is_valid():
+                shipping = form.save(commit=False)
+                shipping.user = request.user
+                shipping.order = create_order
+                shipping.save()
+
+            # 결재 정보 저장
             create_payment = Payment(order=create_order)
             create_payment.imp_uid = request.POST["imp_uid"]
             create_payment.save()
@@ -59,9 +71,10 @@ def payment_process(request):
                 product = Product.objects.get(id=product_id)
                 cart.remove(product)
 
-            messages.success(request, "결제가 완료 되었습니다")
-            return HttpResponse("SUCCESS")
+            messages.success(request, "결재가 완료 되었습니다")
+            # dev_26_2
+            return redirect("/")
 
         else:
-            messages.success(request, "결제금액이 맞지 않아 취소 되었습니다")
+            messages.success(request, "결재금액이 맞지 않아 취소 되었습니다")
             return redirect("/")
